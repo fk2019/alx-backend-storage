84292c6895ee1e4af800c6ece196a607bbd24f91
fk2019 <fk2019@gmail.com>
2023-11-26 10:10:40 +0300

    Create table with an enum attribute

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,9 @@
+-- create users table with id, unique email, name
+-- and country with enum attribute
+CREATE TABLE IF NOT EXISTS users(
+id INT AUTO_INCREMENT,
+email VARCHAR(255) NOT NULL UNIQUE,
+name VARCHAR(255),
+country ENUM('US', 'CO', 'IN') DEFAULT 'US' NOT NULL,
+PRIMARY KEY(id)
+);
