c3de28d02db61ad13d7dd35cbde352994217ffb4
fk2019 <fkmuiruri8@gmail.com>
2023-11-27 11:55:35 +0300

    add update

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""update all topics"""
+
+def update_topics(mongo_collection, name, topics):
+    """ update topics"""
+    result = mongo_collection.update_one(
+        {"name": name}, {"$set": {"topics": topics}}, upsert=True)
+    return result
