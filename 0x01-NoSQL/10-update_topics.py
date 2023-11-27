c30071c6c881686cc2f1fb79501980b8b18261b5
fk2019 <fkmuiruri8@gmail.com>
2023-11-27 12:00:40 +0300

    add insert

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -4,5 +4,5 @@
 def update_topics(mongo_collection, name, topics):
     """ update topics"""
     result = mongo_collection.update_one(
-        {"name": name}, {"$set": {"topics": topics}}, upsert=True)
+        {"name": name}, {"$set": {"topics": topics}})
     return result
