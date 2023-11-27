ed82ab1ef3afeea7cbc8d02f4a2f520fdeb604d4
fk2019 <fkmuiruri8@gmail.com>
2023-11-27 12:55:50 +0300

    add log stats

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""log nginx logs"""
+from pymongo import MongoClient
+
+if __name__ == "__main__":
+    with MongoClient() as client:
+        collection = client.logs.nginx
+        print("{} logs".format(collectiomln.estimated_document_count()))
+        print("Methods:")
+        for method in ["GET", "POST", "PUT", "PATCH","DELETE"]:
+            count = collection.count_documents({"method": method})
+            print("/tmethod {}. {}".format(method, count))
+            status_count = collection.count_documents(
+                {"method": "GET", "path": "/status"})
+            print("{} status check".format(status_count))
