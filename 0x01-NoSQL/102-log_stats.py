6047ca6b1e16e3346233268129f7258750c2c796
fk2019 <fkmuiruri8@gmail.com>
2023-11-27 15:11:14 +0300

    add avg score students

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -1,43 +1,24 @@
 #!/usr/bin/env python3
-""" 12. Log stats
-"""
-
-
+"""log nginx logs"""
 from pymongo import MongoClient
 
-
-def log_stats():
-    """ log_stats.
-    """
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    logs_collection = client.logs.nginx
-    total = logs_collection.count_documents({})
-    get = logs_collection.count_documents({"method": "GET"})
-    post = logs_collection.count_documents({"method": "POST"})
-    put = logs_collection.count_documents({"method": "PUT"})
-    patch = logs_collection.count_documents({"method": "PATCH"})
-    delete = logs_collection.count_documents({"method": "DELETE"})
-    path = logs_collection.count_documents(
-        {"method": "GET", "path": "/status"})
-    print(f"{total} logs")
-    print("Methods:")
-    print(f"\tmethod GET: {get}")
-    print(f"\tmethod POST: {post}")
-    print(f"\tmethod PUT: {put}")
-    print(f"\tmethod PATCH: {patch}")
-    print(f"\tmethod DELETE: {delete}")
-    print(f"{path} status check")
-    print("IPs:")
-    sorted_ips = logs_collection.aggregate(
+if __name__ == "__main__":
+    with MongoClient() as client:
+        collection = client.logs.nginx
+        print("{} logs".format(collection.count_documents({})))
+        print("Methods:")
+        for method in ["GET", "POST", "PUT", "PATCH","DELETE"]:
+            count = collection.count_documents({"method": method})
+            print("\tmethod {}: {}".format(method, count))
+        status_count = collection.count_documents(
+                {"method": "GET", "path": "/status"})
+        print("{} status check".format(status_count))
+        sorted_ips = collection.aggregate(
         [{"$group": {"_id": "$ip", "count": {"$sum": 1}}},
          {"$sort": {"count": -1}}])
-    i = 0
-    for s in sorted_ips:
-        if i == 10:
-            break
-        print(f"\t{s.get('_id')}: {s.get('count')}")
-        i += 1
-
-
-if __name__ == "__main__":
-    log_stats()
+        i = 0
+        for s in sorted_ips:
+            if i == 10:
+                break
+            print(f"\t{s.get('_id')}: {s.get('count')}")
+            i += 1
