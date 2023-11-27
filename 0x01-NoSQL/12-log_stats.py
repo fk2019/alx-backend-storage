525b4b50c207a2f3b3fb3c35c8a7e7f6bdb416ce
fk2019 <fkmuiruri8@gmail.com>
2023-11-27 13:50:29 +0300

    update print

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
         print("Methods:")
         for method in ["GET", "POST", "PUT", "PATCH","DELETE"]:
             count = collection.count_documents({"method": method})
-            print("/tmethod {}. {}".format(method, count))
+            print("\tmethod {}: {}".format(method, count))
         status_count = collection.count_documents(
                 {"method": "GET", "path": "/status"})
         print("{} status check".format(status_count))
