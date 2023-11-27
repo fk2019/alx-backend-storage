e80a92cfe30a5fb726d2cf198bdf570812460612
fk2019 <fkmuiruri8@gmail.com>
2023-11-27 12:11:00 +0300

    add find script

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,7 @@
+#!/usr/binenv python3
+"""find schools by given topic"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """find schools by given topic"""
+    return mongo_collection.find({"topics": topic})
